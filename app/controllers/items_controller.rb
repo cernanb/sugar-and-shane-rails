eec0b02838c245574c6e615b77913ed56ebd8755
Cernan Bernardo <github email address>
2019-02-13 23:18:27 -0800

    Add custom attribute writers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,21 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @brand = @item.build_brand
+    @store = @item.build_store
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      redirect_to new_item_path
+    end
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :cost_cents, :listing_price_cents, :brand_id, :store_id, :sold_price_cents, :brand_attributes => [:name], :store_attributes => [:name])
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,20 @@ class Item < ApplicationRecord
     monetize :listing_price_cents
     monetize :sold_price_cents, allow_nil: true
 
-    belongs_to :store
-    belongs_to :brand
+    belongs_to :store, optional: true
+    belongs_to :brand, optional: true
+
+    def brand_attributes=brand_hash
+        if !brand_hash[:name].empty?
+            brand = Brand.find_or_create_by(name: brand_hash[:name])
+            self.brand = brand
+        end
+    end
+
+    def store_attributes=store_hash
+        if !store_hash[:name].empty?
+            store = Store.find_or_create_by(name: store_hash[:name])
+            self.store = store
+        end
+    end
 end
