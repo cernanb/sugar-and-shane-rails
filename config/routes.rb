7f5ee1a7d391a4148318e9d3c4126fc4a448d6bf
cernanb <github email address>
2019-02-14 21:44:34 -0800

    Add index routes for store and brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :new, :create, :update]
-  resources :stores, only: [:new, :create ]
-  resources :brands, only: [:new, :create ]
+  resources :stores, only: [:new, :create, :index ]
+  resources :brands, only: [:new, :create, :index ]
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
