8fe53d8fa1dc3dc569a72c40a2ad9b317d3e5c0c
cernanb <github email address>
2019-02-14 21:42:57 -0800

    Define routes for stores and brands

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :update]
+  resources :stores, only: [:new, :create ]
+  resources :brands, only: [:new, :create ]
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
